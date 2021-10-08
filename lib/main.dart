6daf1dd47c039f8b9805d5cf6b8076cfe1337457
emptym27 <anansit_m@hotmail.com>
2021-10-08 15:30:01 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -38,8 +38,7 @@ int port = 7794;
 
 class _MyHomePageState extends State<MyHomePage> {
   void mqttConnect() async {
-    client = MqttServerClient.withPort(
-        'cctv4rent.thddns.net', 'clientIdentifier123', port);
+    client = MqttServerClient.withPort('cctv4rent.thddns.net', '1234', port);
     client?.keepAlivePeriod = 60;
     client?.autoReconnect = true;
     client?.onConnected = onConnected;
@@ -60,16 +59,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // });
   }
 
-  mqttSubscribe(String topic) {
-    client?.subscribe('topic', MqttQos.exactlyOnce);
-    return client?.updates;
-  }
-
-  publish(String message) {
-    final builder = MqttClientPayloadBuilder();
-    builder.addString('Hello');
-  }
-
+  // Stream<List<MqttReceivedMessage<MqttMessage>>>? mqttSubscribe(String topic) {
+  //   client?.subscribe('topic', MqttQos.exactlyOnce);
+  //   return client?.updates;
+  // }
   void onConnected() {
     log('Connected');
   }
@@ -81,44 +74,107 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-
     mqttConnect();
   }
 
   @override
   Widget build(BuildContext context) {
+    String test = 'hello';
+    // final builder = MqttClientPayloadBuilder();
+    // builder.addString(test);
+    // if (client!.connectionStatus!.state == MqttConnectionState.connected) {
+    // mqttSubscribe('topic');
+    // }
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: StreamBuilder(
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              var mqttRecieveMessage = snapshot.data;
-              MqttPublishMessage recieveMessage = mqttRecieveMessage[0].payload;
-              String payload = MqttPublishPayload.bytesToStringAsString(
-                  recieveMessage.payload.message);
-              return Center(child: Text(payload));
-            }
-            return const Center(child: CircularProgressIndicator());
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              child: Text(
+                test,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            )
+          ],
         ),
+        // child:StreamBuilder(
+        //   stream: mqttSubscribe('topic'),
+        //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+        //     if (snapshot.hasData) {
+        //       var mqttRecieveMessage = snapshot.data;
+        //       MqttPublishMessage recieveMessage = mqttRecieveMessage[0].payload;
+        //       String payload = MqttPublishPayload.bytesToStringAsString(
+        //           recieveMessage.payload.message);
+        //       return Center(child: Text(payload));
+        //     }
+        //     return const Center(child: CircularProgressIndicator());
+        //   },
+        // ),
       ),
-    );
-  }
-
-  Widget _publish() {
-    return Center(
-      // ignore: deprecated_member_use
-      child: RaisedButton(
-        onPressed: () {
-          client?.publishMessage(
-              'topic', MqttQos.exactlyOnce, builder.payload!);
-        },
-        child: const Text('publish'),
-        color: Colors.lightBlue,
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+              bottom: 20,
+              right: 10,
+              child: FloatingActionButton(
+                onPressed: () {
+                  final builder = MqttClientPayloadBuilder();
+                  builder.addString(test);
+                  client?.publishMessage(
+                      'topic', MqttQos.exactlyOnce, builder.payload!);
+                  log('published');
+                },
+                backgroundColor: Colors.redAccent,
+                foregroundColor: Colors.white,
+                splashColor: Colors.lightBlueAccent,
+                tooltip: "press for publish message",
+                child: const Text('publish'),
+              )),
+          Positioned(
+              bottom: 20,
+              left: 30,
+              child: FloatingActionButton(
+                onPressed: () {
+                  client?.subscribe('topic', MqttQos.exactlyOnce);
+                  log('subscribed');
+                },
+                backgroundColor: Colors.redAccent,
+                foregroundColor: Colors.white,
+                splashColor: Colors.lightBlueAccent,
+                tooltip: "press for subscribe to topic",
+                child: const Text('subscribe'),
+              ))
+        ],
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     client?.publishMessage(
+      //         'topic', MqttQos.exactlyOnce, builder.payload!);
+      //          log('published');
+      //   },
+      //   child: const Text('publish'),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
+      // floatingActionButton:FloatingActionButton(
+      //   backgroundColor: Colors.amber,
+      //   foregroundColor: Colors.white,
+      //   splashColor: Colors.lightBlueAccent,
+      //   tooltip: "press for connect server",
+      //   onPressed:(){
+      //     mqttConnect();
+      //     },
+      //   child: const Text("connect to server"),
+      // ),
     );
   }
 }
